26ccfcd54162770d88622591b3c9ee1c4a7b71dd
Mansurxon G'iyosiddinov <mansurxongiyosiddinov4543@gmail.com>
2023-04-17 16:36:57 +0500

    start

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const menu = document.querySelector('.menu'),
+    times = document.querySelector('.times'),
+    bars = document.querySelector('.bars'),
+    body = document.querySelector('body');
+
+bars.addEventListener('click', () => {
+    menu.classList.toggle('active')
+});
+
+times.addEventListener('click', () => {
+    menu.classList.remove('active')
+})
+
+window.oncontextmenu = () => {
+    alert("Siz bu saytni ma'lumotlarini ko'ra olmaysiz!")
+    return false
+}
+
+body.oncopy = () => {
+    alert("Siz bu saytni matnlarini ko'chira olmaysiz!");
+    return false
+}
+
+const loading = document.querySelector('.loading')
+const css = document.querySelector('.css')
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        loading.style.display = "none"
+        css.style.display = "flex";
+        // css.style. = "flex";
+    }, 2000)
+})
